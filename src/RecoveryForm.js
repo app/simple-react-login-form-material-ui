f713078d6f9b0eb0d0927e684ca6a2e7101737da
Andrey Paskal <apaskal@gmail.com>
2021-10-24 21:11:23 +0300

    Reformatting

--- src/RecoveryForm.js
+++ src/RecoveryForm.js
@@ -1,54 +1,32 @@
-import React from 'react';
-import {
-  Box,
-  Button,
-  Paper,
-  TextField,
-} from '@mui/material'
+import React from 'react'
+import { useHistory } from "react-router-dom"
+import { Box, Button, Paper, TextField } from '@mui/material'
 
-import {
-  useHistory
-} from "react-router-dom";
-
-
-export const RecoveryForm = () => {
+export default function RecoveryForm() {
   const history = useHistory()
-
   return (
-    <>
-      <Box sx={{mx:"auto",width:400}}>
-        <Paper>
+    <Box sx={{ mx: "auto", width: 400 }}>
+      <Paper>
+        <Box
+          sx={{
+            display: 'grid', gap: 2, mt: 1, p: 3,
+            gridTemplateColumns: 'repeat(1, 1fr)',
+          }}
+        >
+          <TextField label="Recovery Email" autoFocus/>
           <Box
-            sx={{
-              display: 'grid',
-              gap: 2,
-              mt: 1,
-              gridTemplateColumns: 'repeat(1, 1fr)',
-              p:3,
-            }}
-          >
-            <TextField label="Recovery Email" autoFocus></TextField>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'row-reverse',
-                gap:2
-              }}
+            sx={{ display: 'flex', flexDirection: 'row-reverse', gap: 2 }} >
+            <Button variant='contained' color='error' >
+              Recovery
+            </Button>
+            <Button variant='contained' color='primary'
+              onClick={() => history.push('/login')}
             >
-              <Button variant='contained' color='error' >
-                Recovery
-              </Button>
-              <Button variant='contained' color='primary'
-                onClick={()=> history.push('/login')}
-              >
-                Go back
-              </Button>
-            </Box>
+              Go back
+            </Button>
           </Box>
-        </Paper>
-      </Box>
-    </>
-  );
-};
-
-export default RecoveryForm
+        </Box>
+      </Paper>
+    </Box>
+  )
+}
--- src/UserAccountPage.js
+++ src/UserAccountPage.js
@@ -1,51 +1,30 @@
-import React from 'react';
-import {
-  Box,
-  Button,
-  Paper,
-  Typography,
-} from '@mui/material'
+import React from 'react'
+import { Box, Button, Paper, Typography } from '@mui/material'
+import { useHistory } from "react-router-dom"
 
-import {
-  useHistory
-} from "react-router-dom";
-
-export const UserAccountPage = () => {
+export default function UserAccountPage() {
   const history = useHistory()
   return (
-    <>
-      <Box sx={{mx:"auto",width:400}}>
-        <Paper>
-          <Box
-            sx={{
-              display: 'grid',
-              gap: 2,
-              mt: 1,
-              gridTemplateColumns: 'repeat(1, 1fr)',
-              p:3,
-            }}
-          >
-            <Typography variant="h5" gutterBottom component="div">
-              User account information
-            </Typography>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'row-reverse',
-                gap:2
-              }}
+    <Box sx={{ mx: "auto", width: 400 }}>
+      <Paper>
+        <Box
+          sx={{
+            display: 'grid', gap: 2, mt: 1, p: 3,
+            gridTemplateColumns: 'repeat(1, 1fr)',
+          }}
+        >
+          <Typography variant="h5" gutterBottom component="div">
+            User account information
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row-reverse', gap: 2 }} >
+            <Button variant='contained' color='primary'
+              onClick={() => history.push('/login')}
             >
-              <Button variant='contained' color='primary'
-                onClick={()=> history.push('/login')}
-              >
-                Exit
-              </Button>
-            </Box>
+              Exit
+            </Button>
           </Box>
-        </Paper>
-      </Box>
-    </>
-  );
-};
-
-export default UserAccountPage
+        </Box>
+      </Paper>
+    </Box>
+  )
+}
