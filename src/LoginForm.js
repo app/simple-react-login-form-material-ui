be7176c3d2c38e46490c6f5a5d5b7a56fb410b44
Andrey Paskal <apaskal@gmail.com>
2021-10-24 20:00:38 +0300

    Reformatting

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,56 +1,36 @@
-import React from 'react';
-import {
-  Box,
-  Button,
-  Paper,
-  TextField,
-  Link,
-} from '@mui/material'
+import React from 'react'
+import { Box, Button, Paper, TextField, Link } from '@mui/material'
+import { useHistory } from "react-router-dom"
 
-import {
-  useHistory,
-} from "react-router-dom";
-
-export const LoginForm = () => {
+export default function LoginForm() {
   const history = useHistory()
   return (
-    <>
-      <Box sx={{mx:"auto",width:400}}>
-        <Paper>
-          <Box
-            sx={{
-              display: 'grid',
-              gap: 2,
-              mt: 1,
-              gridTemplateColumns: 'repeat(1, 1fr)',
-              p:3,
-            }}
-          >
-            <TextField label="Username/Email" autoFocus></TextField>
-            <TextField label="Password" type={'password'}></TextField>
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'row-reverse',
-                gap:2
+    <Box sx={{ mx: "auto", width: 400 }}>
+      <Paper>
+        <Box
+          sx={{
+            display: 'grid', gap: 2, mt: 1, p: 3,
+            gridTemplateColumns: 'repeat(1, 1fr)',
+          }}
+        >
+          <TextField label="Username/Email" autoFocus />
+          <TextField label="Password" type={'password'} />
+          <Box sx={{ display: 'flex', gap: 2, flexDirection: 'row-reverse' }} >
+            <Button variant='contained' color='primary'
+              onClick={() => history.push('/')}
+            >
+              Login
+            </Button>
+            <Link href="#"
+              onClick={(event) => {
+                event.preventDefault(); history.push('/recovery')
               }}
             >
-              <Button variant='contained' color='primary'
-                onClick={()=> history.push('/')}
-              >
-                Login
-              </Button>
-              <Link href="#"
-                onClick={(event)=> { event.preventDefault(); history.push('/recovery') }}
-              >
-                Forgot password?
-              </Link>
-            </Box>
+              Forgot password?
+            </Link>
           </Box>
-        </Paper>
-      </Box>
-    </>
-  );
-};
-
-export default LoginForm
+        </Box>
+      </Paper>
+    </Box>
+  )
+}
