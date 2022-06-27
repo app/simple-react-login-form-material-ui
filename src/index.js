5d41e110d2dc156a5b598d3f4909e45fe01b996f
Andrey Paskal <apaskal@gmail.com>
2022-06-27 09:10:55 +0300

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>
-  ,rootElement
+  </Router>,
+  rootElement
 );
