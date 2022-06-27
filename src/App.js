1cfbb76a7208e8e1d7592d27cea7135ba2768d72
Andrey Paskal <apaskal@gmail.com>
2022-06-27 09:11:20 +0300

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
-import { ModeSwitch, useModeSwitch, ModeSwitchProvider } from './ModeSwitch'
-import LoginForm from './LoginForm.js'
-import RecoveryForm from './RecoveryForm.js'
-import UserAccountPage from './UserAccountPage.js'
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { ModeSwitch, useModeSwitch, ModeSwitchProvider } from "./ModeSwitch";
+import LoginForm from "./LoginForm.js";
+import RecoveryForm from "./RecoveryForm.js";
+import UserAccountPage from "./UserAccountPage.js";
 
 function ThemeManager({ children }) {
-  const [mode] = useModeSwitch()
-  const theme = createTheme({ palette: { mode } })
-  return (
-    <ThemeProvider theme={theme}>
-      {children}
-    </ThemeProvider>
-  )
+  const [mode] = useModeSwitch();
+  const theme = createTheme({ palette: { mode } });
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
 export const App = () => {
@@ -35,7 +31,7 @@ export const App = () => {
         </Switch>
       </ThemeManager>
     </ModeSwitchProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
